3188e717d210d1da9b9d39203e95fc2e60588654
An Le <anl@uark.edu>
2024-07-26 02:09:26 -0500

    dims

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from transformers import BertTokenizer
 # In[ ]:
 
 
-BATCH_SIZE = 32
+BATCH_SIZE = 2
 MAX_SEQ_LEN = 256
 NUM_EPOCHS = 10
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -48,6 +48,7 @@ class YelpDataset(Dataset):
 		
 		input_ids = inputs['input_ids'].squeeze()
 		attention_mask = inputs['attention_mask'].squeeze() # mask pads.
+		attention_mask = torch.where(attention_mask == 1, torch.tensor(0.0), torch.tensor(float('-inf')))
 		
 		return {"input_ids": input_ids, "attention_mask": attention_mask}, label
 
@@ -69,12 +70,8 @@ test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
 # In[ ]:
 
 
-import torch
-import torch.nn as nn
-import math
-
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, max_len=5000):
+    def __init__(self, d_model, max_len=512):
         super(PositionalEncoding, self).__init__()
         
         # Create a matrix of shape (max_len, d_model) to store the positional encodings
@@ -123,15 +120,14 @@ class Classifier(nn.Module):
 
 		# classifier
 		self.linear1 = nn.Linear(word_embed_size, word_embed_size)
-		self.linear2 = nn.Linear(word_embed_size, 3)
+		self.linear2 = nn.Linear(word_embed_size, 5)
 	
 	def forward(self, input, src_mask = None):
 		out = self.embedding(input)
 		out = self.positional(out)
-
-		out = self.encoder(out)
-
-		out = torch.mean(out, dim = 0)
+		
+		out = self.encoder(out, src_key_padding_mask = src_mask)
+		out = torch.mean(out, dim = 1)
 		out = torch.relu(self.linear1(out))
 		out = torch.softmax(self.linear2(out), dim = -1)
 
@@ -155,14 +151,15 @@ for epoch in range(NUM_EPOCHS):
 
 	running_loss = 0.0
 	with tqdm(total=len(train_dataloader), desc=f'Epoch {epoch+1}/{NUM_EPOCHS}', unit='batch', ncols=100) as pbar:
-		for i, data in enumerate(train_dataloader):
-			inputs, labels = data
-			inputs, labels = inputs.to(device), labels.to(device)
+		for data, label in train_dataloader:
+			input_ids = data['input_ids'].to(device)
+			mask = data['attention_mask'].to(device)
+			label = label.to(device)
 
 			optimizer.zero_grad()
 
-			outputs = model(inputs)
-			loss = loss_fn(outputs, labels)
+			outputs = model(input_ids, mask)
+			loss = loss_fn(outputs, label)
 			loss.backward()
 			optimizer.step()
 
@@ -173,10 +170,4 @@ for epoch in range(NUM_EPOCHS):
 	#print(f'\r {running_loss/len(dataloader)}', end='', flush=True)
 
 print('\nFinished Training')
-
-
-# In[ ]:
-
-
-
 
